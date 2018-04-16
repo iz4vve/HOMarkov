bcb484657c0fb41ff22592d18c4d8bab9b9c2af2
Pietro Mascolo <pietro_mascolo@optum.com>
2018-04-16 09:54:20 +0100

    UPDATES setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="HOMarkov",
-    version="0.4.7",
+    version="0.5.0",
     url="https://github.com/iz4vve/HOMarkov",
     download_url="",
     author="Pietro Mascolo",
